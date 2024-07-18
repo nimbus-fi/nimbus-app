3ad206adb457b4c8008a45d726be7cc003ed7e0b
0xShikhar <shikhar@iiitmanipur.ac.in>
2024-07-19 02:33:17 +0530

    feat: add educhain config

---
+++ src/lib/opencampus.ts
@@ -0,0 +1,25 @@
+import { cookieStorage, createConfig, createStorage, http } from "wagmi";
+import { Chain } from "wagmi/chains";
+import { injected } from "wagmi/connectors";
+
+export const opencampus = {
+    id: 656476,
+    name: "opencampus",
+    rpcUrls: {
+        public: { http: ["https://rpc.open-campus-codex.gelato.digital"] },
+        default: { http: ["https://rpc.open-campus-codex.gelato.digital"] },
+    },
+    blockExplorers: {
+        default: {
+            name: "Open Campus Codex",
+            url: "https://opencampus.gelatoscout.com",
+            apiUrl: "https://opencampus.gelatoscout.com/api",
+        },
+    },
+    nativeCurrency: {
+        decimals: 18,
+        name: "EDU",
+        symbol: "EDU",
+    },
+    testnet: true,
+} as const satisfies Chain;
