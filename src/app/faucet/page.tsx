a102ab1519b52bff6370dafaa866c15dd179278d
0xShikhar <shikhar@iiitmanipur.ac.in>
2024-08-07 19:38:55 +0530

    feat: add faucet for users

---
+++ src/app/faucet/page.tsx
@@ -0,0 +1,109 @@
+"use client";
+import React, { useState, useEffect } from 'react';
+import { useAccount } from 'wagmi'
+import { ethers } from 'ethers';
+import { NIMBUS_TOKEN_ADDRESS } from '@/lib/contract';
+import NimbusABI from '@/lib/abi/NimbusToken.json';
+import { toast, ToastContainer } from 'react-toastify';
+import "react-toastify/dist/ReactToastify.css";
+
+const Faucet: React.FC = () => {
+    const account = useAccount()
+    const address = account.address;
+
+    const [loading, setLoading] = useState(false);
+
+    const getTokenContract = () => {
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        if (!provider) throw new Error("No Web3 Provider");
+        return new ethers.Contract(NIMBUS_TOKEN_ADDRESS, NimbusABI.abi, provider.getSigner());
+    };
+
+    const vote = async (proposalId: number, support: boolean) => {
+        setLoading(true);
+        try {
+            const contract = getGovernanceContract();
+            console.log("Voting on proposal:", proposalId, support);
+            const tx = await contract.vote(proposalId, support);
+            console.log("Transaction sent:", tx.hash);
+            const receipt = await tx.wait();
+            console.log("Transaction confirmed:", receipt);
+            toast.success("Voted Successfully!");
+
+            toast(
+                <div>
+                    Link - {`https://opencampus-codex.blockscout.com/tx/${tx.hash}`}
+                </div>
+            );
+
+            await fetchProposals();
+        } catch (error) {
+            console.error("Error voting:", error);
+            toast.error("Failed to vote!");
+
+            toast(
+                <div>
+                    {(error as any)?.reason || "Unknown error occurred"}
+                </div>
+            );
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    const executeProposal = async () => {
+        setLoading(true);
+        try {
+            const contract = getTokenContract();
+            const tx = await contract.faucet();
+            console.log("Transaction sent:", tx.hash);
+            const receipt = await tx.wait();
+            console.log("Transaction confirmed:", receipt);
+            toast.success("Proposal Executed Successfully!");
+
+            toast(
+                <div>
+                    Link - {`https://opencampus-codex.blockscout.com/tx/${tx.hash}`}
+                </div>
+            );
+        } catch (error) {
+            console.error("Error executing proposal:", error);
+            toast.error("Failed to get tokens!");
+
+            toast(
+                <div>
+                    {(error as any)?.reason || "Unknown error occurred"}
+                </div>
+            );
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    if (!account.isConnected) {
+        return (
+            <div className="flex items-center align-middle min-h-screen text-center justify-center text-4xl font-bold">
+                <div>
+                    Please connect your wallet.
+                </div>
+            </div>
+        );
+    }
+
+    return (
+        <div className="container mx-auto px-32 py-16">
+            <h1 className="text-3xl font-bold mb-6">Faucet</h1>
+
+            <div className="bg-white p-6 rounded-xl shadow-xl ">
+                <div className="bg-white p-6 rounded-xl ">
+                    <h2 className="text-2xl font-semibold mb-4">Get 10 NIBS token for testing</h2>
+                    <button onClick={executeProposal}> Get Token</button>
+                </div>
+            </div>
+
+            <ToastContainer position="top-right" autoClose={5000} />
+        </div >
+    );
+};
+
+export default Faucet;
